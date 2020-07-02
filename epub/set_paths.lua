972cdf14a444f595591b83b29ad57a11f4c6153d
Wen Kokke <wenkokke@users.noreply.github.com>
2020-07-03 00:23:11 +0100

    removed superfluous lua script, moved acknowledgements

--- epub/set_paths.lua
+++
@@ -1,4 +0,0 @@
--- set_paths.lua
-local version = _VERSION:match("%d+%.%d+")
-package.path = 'lua_modules/share/lua/' .. version .. '/?.lua;lua_modules/share/lua/' .. version .. '/?/init.lua;' .. package.path
-package.cpath = 'lua_modules/lib/lua/' .. version .. '/?.so;' .. package.cpath
