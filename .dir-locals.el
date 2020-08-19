39a686500a27182299b9825fcbc9ab9eff7652b2
Wen Kokke <wenkokke@users.noreply.github.com>
2020-08-19 22:44:21 +0200

    Added Aquamacs-specific patch.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,11 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-;; Disable auto-fill-mode for PLFA
+;; Disable auto-fill
 (turn-off-auto-fill)
 (remove-hook 'text-mode-hook #'turn-on-auto-fill)
+
+
+;; Disable auto-fill for Aquamacs
+(when (featurep 'aquamacs)
+  (remove-hook 'text-mode-hook 'auto-detect-wrap))
