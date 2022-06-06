005a9ba6d53d45e160791412d1089fa1dc8d9dd1
Wen Kokke <wenkokke@users.noreply.github.com>
2022-06-06 15:35:09 +0100

    Removed Lua

--- book/lua/remove-badges.lua
+++
@@ -1,8 +0,0 @@
-function Link(ln)
-  -- Remove link elements if their first child is a badge.
-	if #ln.content > 0 and ln.content[1].t == "Image" then
-    if string.find(ln.content[1].src, "https://img.shields.io/badge/", 1, true) then
-      return pandoc.Null()
-    end
-	end
-end
