34247e049e0584cf10c51cb9cd85c8a7231fccc8
Wen Kokke <wenkokke@users.noreply.github.com>
2022-06-10 17:03:19 +0100

    Started on #676

--- tools/Buildfile/QualifiedID.hs
+++ tools/Buildfile/QualifiedID.hs
@@ -1,2 +1,15 @@
 module Buildfile.QualifiedID where
 
+import Text.HTML.TagSoup
+import Data.Text (Text)
+
+-- TODO: prefix all id attributes in Agda code with "agda:<modulename>:"
+-- TODO: unless the id is numeric
+
+isAgdaOpen :: Tag Text -> Bool
+isAgdaOpen (TagOpen tag attr) = tag == "pre" && ("class", "agda") `elem` attr
+isAgdaOpen _ = False
+
+isAgdaClose :: Tag Text -> Bool
+isAgdaClose = isTagCloseName "pre"
+
