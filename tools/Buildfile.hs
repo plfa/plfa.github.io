c7a11ff35cb9d6e4ebecb5abe0551e2f09921fa9
Wen Kokke <wenkokke@users.noreply.github.com>
2022-06-19 01:09:02 +0100

    Update Buildfile.hs

--- tools/Buildfile.hs
+++ tools/Buildfile.hs
@@ -754,7 +754,7 @@ urlToIdent url =
 npx :: (HasCallStack, CmdResult r) => [CmdOption] -> String -> [String] -> Action r
 npx cmdOpts package args = do
   need ["package.json"]
-  command (AddEnv "npm_config_yes" "true" : Traced package : cmdOpts) "npx" (package : args)
+  command (AddEnv "npm_config_yes" "true" : Traced package : cmdOpts) ("npx" <.> exe) (package : args)
 
 sass :: (HasCallStack, CmdResult r) => [CmdOption] -> [String] -> Maybe LazyText.Text -> Action r
 sass cmdOpts args maybeStdin = do
